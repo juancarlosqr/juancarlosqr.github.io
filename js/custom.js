6ad1d02c95fc83888b889bf14b23b349abac7831
Juan Carlos Quintero <jcquintero.ce@gmail.com>
2014-02-25 23:56:03 -0430

    se agrego el nuevo tema

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+	// prettyPhoto
+	$("a[class^='prettyPhoto']").prettyPhoto();
+
+	// plugin carousel
+	$('.carousel').carousel();
+
+	// jQuery onePageNav 
+	$('.navbar-nav').onePageNav({
+		currentClass: 'active',
+		changeHash: false,
+		scrollSpeed: 750,
+		scrollOffset: 94,
+		scrollThreshold: 0.1,
+		easing: 'swing',
+	});
+
+	// jquery plugin cycle
+	$('.list-testimonial').cycle({
+		slides: 'li',
+		pager: ".testimonial-bullets",
+		timeout: 0,
+		autoHeight: 0,
+		centerHorz: true
+	});
+
+	// size testimonial 
+	$('.list-testimonial').on( 'cycle-update-view', function(e,o,sh,cs) {
+		var $this = $(this);
+
+        $this.animate({
+          height: $(cs).height()
+        }, 500);
+
+        $(window).resize(function() {
+          $this.stop().animate({
+            height: $(cs).height()
+          }, 500);
+        });
+      });
+});
